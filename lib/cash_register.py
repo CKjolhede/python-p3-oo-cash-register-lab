ff96c7f576c095b79f34049f869aa3422fcc111c
ckjolhede <ckjolhede@gmail.com>
2024-06-16 02:20:00 -0600

    working

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ipdb
 
 class CashRegister:
   
@@ -6,12 +7,13 @@ class CashRegister:
     self.total = 0
     self.discount = discount
     self.items = []
+    self.previous_transactions = {}
 
   def add_item(self, name, price = 0, quantity = 1):
     self.total = self.total + (price * quantity) 
     for _ in range(quantity):
         self.items.append(name)
-    return
+    self.previous_transactions = ({"item":name, "price":price, "quantity":quantity})
 
   def apply_discount(self):
     self.total = self.total * ((100 - self.discount)/100)
@@ -19,3 +21,18 @@ class CashRegister:
       print(f'After the discount, the total comes to ${int(self.total)}.') 
     else: 
       print("There is no discount to apply.")
+      
+  def void_last_transaction(self):
+    self.total -= self.previous_transactions["price"] * self.previous_transactions["quantity"]    
+    self.previous_transactions.pop()
+    return
+  
+new_register = CashRegister(10)
+cash_register = CashRegister()
+cash_register_with_discount = CashRegister(20)
+new_register.add_item("eggs", 1.99)
+new_register.add_item("tomato", 1.76)
+cash_register.add_item("Lucky Charms", 4.5)
+cash_register.add_item("Ritz Crackers", 5.0)
+cash_register.add_item("Justin's Peanut Butter Cups", 2.50, 2)
+ipdb.set_trace()
