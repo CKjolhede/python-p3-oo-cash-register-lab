7f226d80d2afea8286fe873b9d667100eaaa62be
ckjolhede <ckjolhede@gmail.com>
2024-06-15 19:31:31 -0600

    working

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  
+  def __init__(self, discount = 0):
+    self.total = 0
+    self.discount = discount
+    self.items = []
+
+  def add_item(self, name, price = 0, quantity = 1):
+    self.total = self.total + (price * quantity) 
+    for _ in range(quantity):
+        self.items.append(name)
+    return
+
+  def apply_discount(self):
+    self.total = self.total * ((100 - self.discount)/100)
+    if self.discount > 0:
+      print(f'After the discount, the total comes to ${int(self.total)}.') 
+    else: 
+      print("There is no discount to apply.")
